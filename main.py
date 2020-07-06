f2546f55b8e13263fd9297cc01d795f52aa7707b
TravisEntner <Travisentner@gmail.com>
2020-07-06 21:53:15 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+import turtle
+import random
+
+player_one = turtle.Turtle()
+player_one.color("green")
+player_one.shape("turtle")
+player_one.penup()
+player_one.goto(-200,100)
+player_2 = player_one.clone()
+player_2.color("blue")
+player_2.penup()
+player_2.goto(-200,100)
+
+
+player_one
+player_one
+player_one
+player_one
+player_one
+player_one
